b6247b0d83858f5bb2c275305b18851b1c07d36a
Michal <abxvxxl@gmail.com>
2025-06-27 15:27:10 +0200

    Added custom NewsApiException exception

--- src/main/java/com/majkel/emotinews/MainApp.java
+++ src/main/java/com/majkel/emotinews/MainApp.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class MainApp {
     public static void main(String []args){
         NewsFetcher newsFetcher=new NewsFetcher();
-        List<NewsArticle>articles = newsFetcher.getNewsList();
+        List<NewsArticle>articles = newsFetcher.getNewsList("top-headlines?q=Bitcoin");
         for(NewsArticle article: articles){
             System.out.println(article+" ");
         }
--- src/main/java/com/majkel/emotinews/NewsFetcher.java
+++ src/main/java/com/majkel/emotinews/NewsFetcher.java
@@ -1,6 +1,7 @@
 package com.majkel.emotinews;
 
 import com.google.gson.Gson;
+import com.majkel.emotinews.exception.NewsApiException;
 
 import java.io.IOException;
 import java.net.URI;
@@ -8,33 +9,31 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.sql.SQLOutput;
 import java.util.List;
 
 public class NewsFetcher {
     private static final HttpClient httpClient=HttpClient.newHttpClient();
 
-    public List<NewsArticle> getNewsList(){
+    public List<NewsArticle> getNewsList(String query){
         Gson gson=new Gson();
         List<NewsArticle> articles = null;
         try {
             HttpRequest getRequest = HttpRequest.newBuilder()
-                    .uri(new URI("https://newsapi.org/v2/top-headlines?q=Bitcoin"))
+                    .uri(new URI("https://newsapi.org/v2/"+query))
                     .header("X-Api-Key", ConfigLoader.getValue("api.news.key"))
                     .build();
             HttpResponse<String> getResponse=httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
-            if(getResponse.statusCode()!=200){
-                System.out.println("ERROR: Response code is "+getResponse.statusCode());
-                return List.of();
-            }
+            if(getResponse.statusCode()!=200)
+                throw new NewsApiException("Received status code "+getResponse.statusCode());
+
             NewsHolder response=gson.fromJson(getResponse.body(),NewsHolder.class);
             articles=response.getArticles();
             System.out.println("Kod: "+response.getStatus());
 
         }catch(URISyntaxException e){
-            System.out.println("ERROR: URISyntaxException "+e.getMessage());
-        }catch (InterruptedException | IOException ie){
-            System.out.println("Error: "+ie.getMessage());
+            throw  new RuntimeException("URISyntaxException ",e);
+        }catch (InterruptedException | IOException e){
+            throw new NewsApiException("Error while calling NewsAPI",e);
         }
         return articles!=null? articles:List.of();
     }
