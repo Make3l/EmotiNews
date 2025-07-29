9a5b12a2735d82cae412f36dc4904118b1b89b1c
Michal <abxvxxl@gmail.com>
2025-07-29 03:01:49 +0200

    Fixed build bug

--- src/main/java/com/majkel/emotinews/ui/fxapp/FXMainApp.java
+++ src/main/java/com/majkel/emotinews/ui/fxapp/FXMainApp.java
@@ -2,23 +2,21 @@ package com.majkel.emotinews.ui.fxapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
-public class FXMainApp extends Application{
+public class FXMainApp extends Application {
     @Override
-    public void start(Stage stage) throws Exception{
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("/view/MainView.fxml"));
-        Parent root=loader.load();
-        Scene scene=new Scene(root);
-
-        stage.setTitle("EmotiNews");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/majkel/emotinews/ui/view/MainView.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("EmotiNews");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 }
