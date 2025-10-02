048a6c8f7bce70ec986290e9839d693ce4fc2bc0
Michal <abxvxxl@gmail.com>
2025-10-02 20:56:40 +0200

    Added InputSanitizer class that has static method - filterTopic, that deletes excessive spaces, backlashes, invisible control characters and trims to 128 code points

---
+++ src/main/java/com/majkel/emotinews/utils/InputSanitizer.java
@@ -0,0 +1,26 @@
+package com.majkel.emotinews.utils;
+
+public class InputSanitizer {
+
+    private static final int MAX_CODEPOINTS=128;
+
+    public static String filterTopic(String topic){
+        String result=topic.trim();
+
+        result=result.replaceAll("\\s+"," ");//replacing multiple white characters with a single space
+
+        result=result.replace("\\", "");// deletes any \ with
+
+        result=result.replaceAll("\\p{C}","");// \p{C} = invisible control characters
+
+
+        int codePoints=result.codePointCount(0,result.length());
+        if(codePoints>MAX_CODEPOINTS){
+            int end=result.offsetByCodePoints(0,MAX_CODEPOINTS);
+            result=result.substring(0,end);
+        }
+
+        return result;
+    }
+
+}
